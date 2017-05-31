b870eaedf97502d004e070fd243aefee11378694
yumechi <yumechi@users.noreply.github.com>
2017-06-01 00:46:16 +0900

    Hello elixir

---
+++ src/Hello.ex
@@ -0,0 +1,8 @@
+# 参考にした：http://qiita.com/tanaka0325/items/045ce9d9ee9355741c3d
+defmodule Hello do
+  def main do
+    IO.puts "にこにこにー"
+  end
+end
+
+Hello.main
---
+++ src/execute_memo.md
@@ -0,0 +1,5 @@
+# Elixirを実行する
+3つの方法がある
+- `iex` : インタラクティブ実行
+- `elixir Hello.exs` : スクリプトとして実行
+- `elixirc Hello.ex` : コンパイルする 
