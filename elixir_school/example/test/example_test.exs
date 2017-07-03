7f5ec53813042daba851939fa736384513cae02e
yumechi <yumechi@users.noreply.github.com>
2017-07-03 23:59:57 +0900

    add testcase

--- elixir_school/example/test/example_test.exs
+++ elixir_school/example/test/example_test.exs
@@ -6,3 +6,12 @@ defmodule ExampleTest do
     assert 1 + 1 == 2
   end
 end
+
+defmodule OutputTest do
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+
+  test "outputs Hello World" do
+    assert capture_io(fn -> IO.puts "Hello World" end) == "Hell World\n"
+  end
+end
